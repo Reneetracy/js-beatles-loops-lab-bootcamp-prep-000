3d4b2d719e0632372c7455666e8706a85d180d63
Renee Tracy <renee.upandaway@gmail.com>
2017-09-11 20:31:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,10 +9,9 @@ function theBeatlesPlay(musicians, instruments){
 
 function johnLennonFacts(facts){
   var i = 0;
-  while (i < facts.length){
-    facts.push(facts[i] + "!!!");
+  while (i < facts.length) {
     i++;
   }
 
-  return facts
+  return facts + "!!!"
 }

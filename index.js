74ff999230b3df3872b21ecfa67d18a4a052b52b
Renee Tracy <renee.upandaway@gmail.com>
2017-09-11 18:40:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function theBeatlesPlay(musicians, instruments){
   var whoPlaysWhat = [];
   for (var i = 0; i < musicians.length; i++) {
-    whoPlaysWhat.push(musicians[i] + "plays" + instruments[i]);
+    whoPlaysWhat.push(musicians[i] + " plays" + instruments[i]);
   }
 
   return whoPlaysWhat;

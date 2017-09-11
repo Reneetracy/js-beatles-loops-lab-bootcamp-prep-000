ad0a9ad39905b7a1579f340d0f3453a083be4f2b
Renee Tracy <renee.upandaway@gmail.com>
2017-09-11 20:21:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,20 +1,8 @@
 function theBeatlesPlay(musicians, instruments){
   var whoPlaysWhat = [];
   for (var i = 0; i < musicians.length; i++) {
-    whoPlaysWhat.push(musicians[i] + " plays " + instruments[i]);
+    whoPlaysWhat.push('${musicians[i]}' plays '${instruments[i]}')
   }
 
   return whoPlaysWhat;
-}
-
-function johnLennonFacts(facts){
-  var facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"];
-  var newFacts = [];
-  var i = 0;
-  while (i < facts.length){
-    newFacts.push(facts[i] + "!!!");
-    i++;
-  }
-
-  return newFacts;
 }
